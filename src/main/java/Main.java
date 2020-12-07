d796e5d0e7ac8297545486aa2aa2088074d78023
Denis Lazykin <dlazykin856@gmail.com>
2020-12-07 11:07:19 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(5, 5));
+        System.out.println(calc.mult(11, 22));
+        System.out.println(calc.pow(5, 15));
+    }
+}
